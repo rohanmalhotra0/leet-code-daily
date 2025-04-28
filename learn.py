cf8be5822b2b893aba9b632a01c9ab949a19c68a
Rohan Malhotra <104740676+rohanmalhotra0@users.noreply.github.com>
2025-04-28 08:42:00 -0400

    learn py where I go back and look over basic concepts

---
+++ learn.py
@@ -0,0 +1,15 @@
+# adding all of alphabet in a map 
+map1 = {i + 1: chr(ord('A') + i) for i in range(26)}
+
+#binary search
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
